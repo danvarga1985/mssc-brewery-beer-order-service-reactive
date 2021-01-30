2d59758b1440c6e01497aabc23eddfee7f4e817c
danvarga1985 <danvarga1985@gmail.com>
2021-01-30 18:53:53 +0100

    Initial commit

---
+++ src/test/java/dan/varga/msscbrewerybeerorderservicereactive/MsscBreweryBeerOrderServiceReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package dan.varga.msscbrewerybeerorderservicereactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryBeerOrderServiceReactiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
